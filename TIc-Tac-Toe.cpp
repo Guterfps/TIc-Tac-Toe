a947437795a2e38a8bca2485691f01bc81a0ca47
Guterfps <ithayg@gmail.com.com>
2022-08-07 11:01:27 +0300

    Add project files.

---
+++ TIc-Tac-Toe.cpp
@@ -0,0 +1,16 @@
+// TIc-Tac-Toe.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <ctime>
+
+using std::cout;
+using std::cin;
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
