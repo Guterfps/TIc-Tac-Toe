e606f6aef58283c7ead6d9aea08825057059e70c
Guterfps <ithayg@gmail.com.com>
2022-08-07 11:27:27 +0300

    Bord

--- TIc-Tac-Toe.cpp
+++ TIc-Tac-Toe.cpp
@@ -7,10 +7,51 @@
 using std::cout;
 using std::cin;
 
+void drawBord(char *spaces);
+void playerMove(char *spaces, char player);
+void computerMove(char *spaces, char computer);
+bool checkWinner(char *spaces, char player, char computer);
+bool checkTie(char *spaces);
+
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char spaces[9] = { ' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', };
+	char playr = 'o';
+	char computer = 'x';
+	bool running = true;
+
+	drawBord(spaces);
+
+	
+	return 0;
+
 }
 
+void drawBord(char *spaces) {
+	cout << "\n";
+	for (int i = 0; i < 9; i += 3) {
+		cout << "     |     |     " << '\n';
+		cout << "  " << spaces[i] << "  |  " << spaces[i + 1] << "  |  " << spaces[i + 2] << "  \n";
+		cout << "_____|_____|_____" << '\n';
+		
+	}
+	cout << "\n";
+}
+
+void playerMove(char *spaces, char player) {
+
+}
+
+void computerMove(char *spaces, char computer) {
+
+}
+
+bool checkWinner(char *spaces, char player, char computer) {
+	return 0;
+}
+
+bool checkTie(char *spaces) {
+	return 0;
+}
