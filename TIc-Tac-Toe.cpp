4642a3f13583f58ee76e3d8a965edda8a7b2c651
Guterfps <ithayg@gmail.com.com>
2022-08-07 12:47:23 +0300

    Done

--- TIc-Tac-Toe.cpp
+++ TIc-Tac-Toe.cpp
@@ -82,7 +82,7 @@ void playerMove(char *spaces, char player) {
 			break;
 		}
 		
-	} while (number < 0 || number > 8 || spaces[number] != player || spaces[number] == player);
+	} while (!number > 0 || !number < 8 );
 }
 
 void computerMove(char *spaces, char computer) {
